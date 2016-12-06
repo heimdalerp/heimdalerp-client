454f8091705fcd97467ea8458fedc72a897a48fd
Pablo Zanitti <pzanitti@gmail.com>
2016-12-06 20:48:57 -0300

    Add allow a custom key.

--- src/utils/components/OneToOne.vue
+++ src/utils/components/OneToOne.vue
@@ -2,13 +2,13 @@
 <div>
   <label for="{{id}}">{{name}}:</label>
   <select id="{{id}}" class="form-control" v-model="model">
-    <option v-for="option in options" value="{{ option.id }}">{{ option.name }}</option>
+    <option v-for="option in options" value="{{ option[key] || option.id }}">{{ option.name }}</option>
   </select>
 </div>
 </template>
 <script>
 export default {
-  props: ['name', 'options', 'model'],
+  props: ['key', 'name', 'options', 'model'],
   created: function () {
     this.id = Math.floor((Math.random() * 10000) + 1)
   }
